b277ffa0ee15ad66b775f278f1095b23a1324869
Manuel Hernández <manuel.jhernandez.91@me.com>
2018-05-02 09:32:28 +0100

    React routing excercise. Accessing url params

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { Route, NavLink } from 'react-router-dom';
+// import axios from 'axios';
+import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
+import FullPost from './FullPost/FullPost';
+import './Blog.css';
+
+class Blog extends Component {
+
+  render () {
+
+    // if (this.state.error) { posts = <p style={{textAlign: 'center'}} >Something went wrong!</p>; }
+
+    return (
+      <div className='Blog'>
+        <header>
+          <nav>
+            <ul>
+              <li><NavLink
+                    to="/"
+                    exact
+                    activeClassName="active"
+                    activeStyle={{textDecoration: 'underline'}}
+                    >Home</NavLink>
+              </li>
+              <li>
+                <NavLink to={{
+                  pathname: "/new-post",
+                  hash: '#submit',
+                  search: '?quick-submit=true'
+                  }}>New Post</NavLink>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        {/*<Route path="/" exact render={() => <Posts />} />*/}
+        <Route path="/" exact component={Posts} />
+        <Route path="/new-post" component={NewPost} />
+        <Route path="/post/:id" exact component={FullPost} />
+      </div>
+    );
+  }
+}
+
+export default Blog;
---
+++ src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+import Post from '../../../components/Post/Post';
+import axios from '../../../axios';
+import { Link } from 'react-router-dom';
+import './Posts.css';
+
+class Posts extends Component {
+  state = {
+    posts: []
+  }
+
+  postSelectedHandler= (id) => {
+    this.setState({ selectedPostId: id });
+  }
+
+  componentDidMount() {
+    axios.get('/posts')
+    .then( response => {
+      const posts = response.data.slice(0, 4);
+      const updatedPosts = posts.map( post => {
+        return { ...post, author: 'Manu'}
+      });
+
+      this.setState({posts: updatedPosts});
+    })
+    .catch( error => this.setState({error: true}));
+  }
+
+  render() {
+
+    let posts = this.state.posts.map( post => {
+      return (
+        <Link to={`/post/${post.id}`} key={post.id}>
+          <Post
+            title={post.title}
+            author={post.author}
+            clicked={() => this.postSelectedHandler(post.id)}
+          />
+        </Link>
+      );
+    });
+
+    return (
+      <section className="Posts">
+        {posts}
+      </section>
+    );
+  }
+}
+
+export default Posts;
