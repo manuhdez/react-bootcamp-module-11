2c1f5027bd06689fc4a58f0fa6a67ba3213750bb
Manuel Hernández <manuel.jhernandez.91@me.com>
2018-05-02 12:30:56 +0100

    Added async component loading

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 // import axios from 'axios';
-import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
-// import FullPost from './FullPost/FullPost';
 import './Blog.css';
+import Posts from './Posts/Posts';
+// import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+const AsyncNewPost = asyncComponent(() => import('./NewPost/NewPost'));
+// import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
 
@@ -39,7 +41,7 @@ class Blog extends Component {
         </header>
         {/*<Route path="/" exact render={() => <Posts />} />*/}
         <Switch>
-          { this.state.auth ? <Route path="/new-post" component={NewPost} /> : null }
+          { this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null }
           <Route path="/posts" component={Posts} />
           <Route render={() => <h1>Page not found</h1>} />
           {/*<Redirect from="/" to="/posts" />*/}
